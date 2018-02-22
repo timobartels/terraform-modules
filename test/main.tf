5b179527055a41ca10bff61af7716d1d8249d608
Timo Bartels <timo_bartels@mckinsey.com>
2018-02-22 08:22:38 +0000

    removing duplicate terraform block

--- test/main.tf
+++ test/main.tf
@@ -1,9 +1,5 @@
 terraform {
     required_version = ">= 0.10, < 0.12"
-}
-
-terraform {
-    required_version = ">= 0.10, < 0.12"
     backend "s3" {
         region   = "${var.environment_region}"
         key      = "sample/${var.environment_name}/test/terraform.tfstate"
