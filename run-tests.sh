bf7f2ff77ccd30383bc9a3cab63efc5be673b6a8
Timo Bartels <timo@bartels-online.net>
2018-03-20 18:43:40 +0100

    more testing

--- run-tests.sh
+++ run-tests.sh
@@ -9,17 +9,18 @@ export GIT_API_URL="https://api.github.com/repos/$org/$repo/pulls/$pr"
 export mergebase=`curl $GIT_API_URL | jq .base.sha | sed 's/\"//g'`
 
 # Identify files that changed as part of pull request
-export changed_files=`git --no-pager diff --name-only $CIRCLE_BRANCH $mergebase`
+#export changed_files=`git --no-pager diff --name-only $CIRCLE_BRANCH $mergebase`
 
 # Extract directories that were changed
-export folders=`echo $changed_files | cut -d '/' -f1 | uniq`
+#export folders=`echo $changed_files | cut -d '/' -f1 | uniq`
+export folders=`git --no-pager diff --name-only $CIRCLE_BRANCH $mergebase | cut -d '/' -f1 | uniq`
 
-echo "Files changed in this pull request: "
-echo $changed_files
+#echo "Files changed in this pull request: "
+#echo $changed_files
 echo "Folders: "
 echo $folders
 
-for folder in `echo $changed_files | cut -d '/' -f1 | uniq`
+for folder in $folders
 do
     if [ $folder == "test" ];
     then
