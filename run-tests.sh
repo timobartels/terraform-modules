20f6ca2dc913cf5d9668608ee597eba61eda596c
Timo Bartels <timo_bartels@mckinsey.com>
2018-03-20 14:07:44 +0000

    refactoring run-tests-sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,11 +3,10 @@
 # Identify files that changed as part of pull request
 export changed_files=`git --no-pager diff --name-only $CIRCLE_BRANCH $(git merge-base $CIRCLE_BRANCH master)`
 
-export test=`git merge-base test-again master`
+#export test=`git merge-base test-again master`
+export test=155a93dac900bbceb761b1a22bff35a47479b092
 
-echo $test
-
-export out=`git --no-pager diff --name-only test-again $test`
+export changed_files=`git --no-pager diff --name-only test-again $test`
 
 echo $out
 
